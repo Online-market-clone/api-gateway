cd1f0d049baef7d9630302bdbfb905bab16e8a4f
Darshan Shah <darshanshah.sculptsoft@gmail.com>
2020-10-29 21:01:45 +0530

    API gateway configuration with centralized swagger

--- src/main/java/com/emart/proxyservice/ApiGatewayApplication.java
+++ src/main/java/com/emart/proxyservice/ApiGatewayApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
---
+++ src/main/java/com/emart/proxyservice/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.emart.proxyservice.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.RestTemplate;
+
+import com.emart.proxyservice.service.ServiceDefinitionsContext;
+
+import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+	@Autowired
+	private ServiceDefinitionsContext definitionContext;
+
+	@Bean
+	public RestTemplate configureTemplate() {
+		return new RestTemplate();
+	}
+
+	@Primary
+	@Bean
+	@Lazy
+	public SwaggerResourcesProvider swaggerResourcesProvider(InMemorySwaggerResourcesProvider defaultResourcesProvider,
+			RestTemplate temp) {
+		return () -> {
+			List<SwaggerResource> resources = new ArrayList<>(defaultResourcesProvider.get());
+			resources.clear();
+			resources.addAll(definitionContext.getSwaggerDefinitions());
+			return resources;
+		};
+	}
+}
